7d3e971d0d16f810991535af5f5fe4a3d8946851
rautis <aapo@pynnonsaari.net>
2019-02-12 10:13:22 +0200

    Initial Commit

---
+++ RemoveBrowsingData/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RemoveBrowsingData
+//
+//  Created by Aapo Rautiainen on 12/02/2019.
+//  Copyright © 2019 Aapo Rautiainen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
